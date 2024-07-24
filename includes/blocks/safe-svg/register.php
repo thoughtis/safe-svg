935f7161b8ed047bd4d91434a73e3ce4dda3e1ad
Jessica Townsend <jessica@thoughtcatalog.com>
2024-07-24 07:30:44 -0700

    process inline styles outside of sprintf

--- includes/blocks/safe-svg/register.php
+++ includes/blocks/safe-svg/register.php
@@ -67,6 +67,33 @@ function render_block_callback( $attributes ) {
 	 */	
 	$cover_style = apply_filters( 'safe_svg_cover_inline_style', isset( $attributes['alignment'] ) ? 'text-align: ' . $attributes['alignment'] : 'text-align: left');
 	
+	$inside_style = array();
+
+	if ( isset( $attributes['style']['spacing']['padding'] ) ) {
+		$inside_style = add_css_property_prefix( $attributes['style']['spacing']['padding'], 'padding' );
+	}
+	
+	if ( isset( $attributes['style']['spacing']['margin'] ) ) {
+		$inside_style = add_css_property_prefix( $attributes['style']['spacing']['margin'], 'margin' );
+	}
+
+	$inside_style = array_map(
+		fn( $value ) => convert_to_css_variable( $value ),
+		$inside_style
+	);
+
+	$inside_style = array_merge(
+		$inside_style,
+		array(
+			'width'            => isset( $attributes['dimensionWidth'] ) ? esc_attr( $attributes['dimensionWidth'] . 'px' ) : '',
+			'height'           => isset( $attributes['dimensionHeight'] ) ? esc_attr( $attributes['dimensionHeight'] . 'px' ) : '',
+			'background-color' => ( isset( $attributes['backgroundColor'] ) ? esc_attr( 'var(--wp--preset--color--' . $attributes['backgroundColor'] . ')' ) : '' ) ?: ( isset( $attributes['style']['color']['background'] ) ? esc_attr( $attributes['style']['color']['background'] ) : '' ),
+			'color'            => ( isset( $attributes['textColor'] ) ? esc_attr( 'var(--wp--preset--color--' . $attributes['textColor'] . ')' ) : '' ) ?: ( isset( $attributes['style']['color']['text'] ) ? esc_attr( $attributes['style']['color']['text'] ) : '' ),
+		)
+	);
+
+	$inside_style = render_inline_css( $inside_style );
+
 	/**
 	 * The wrapper markup.
 	 *
@@ -83,22 +110,11 @@ function render_block_callback( $attributes ) {
 		'safe_svg_inline_markup',
 		sprintf(
 			'<div class="wp-block-safe-svg-svg-icon safe-svg-cover"%s>
-				<div class="safe-svg-inside%s" style="width: %spx; height: %spx; background-color: var(--wp--preset--color--%s); color: var(--wp--preset--color--%s); padding-top: %s; padding-right: %s; padding-bottom: %s; padding-left: %s; margin-top: %s; margin-right: %s; margin-bottom: %s; margin-left: %s;">%s</div>
+				<div class="safe-svg-inside%s"%s>%s</div>
 			</div>',
 			empty( $cover_style ) ? '' : ' style="' . esc_attr( $cover_style ) . '"',
 			empty( $class_name ) ? '' : ' ' . esc_attr( $class_name ),
-			isset( $attributes['dimensionWidth'] ) ? esc_attr( $attributes['dimensionWidth'] ) : '',
-			isset( $attributes['dimensionHeight'] ) ? esc_attr( $attributes['dimensionHeight'] ) : '',
-			isset( $attributes['backgroundColor'] ) ? esc_attr( $attributes['backgroundColor'] ) : '',
-			isset( $attributes['textColor'] ) ? esc_attr( $attributes['textColor'] ) : '',
-			isset( $attributes['style']['spacing']['padding']['top'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['padding']['top'] ) ) : '',
-			isset( $attributes['style']['spacing']['padding']['right'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['padding']['right'] ) ) : '',
-			isset( $attributes['style']['spacing']['padding']['bottom'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['padding']['bottom'] ) ) : '',
-			isset( $attributes['style']['spacing']['padding']['left'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['padding']['left'] ) ) : '',
-			isset( $attributes['style']['spacing']['margin']['top'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['margin']['top'] ) ) : '',
-			isset( $attributes['style']['spacing']['margin']['right'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['margin']['right'] ) ) : '',
-			isset( $attributes['style']['spacing']['margin']['bottom'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['margin']['bottom'] ) ) : '',
-			isset( $attributes['style']['spacing']['margin']['left'] ) ? esc_attr( convert_to_css_variable( $attributes['style']['spacing']['margin']['left'] ) ) : '',
+			empty( $inside_style ) ? '' : ' style="' . esc_attr( $inside_style ) . '"',
 			$contents
 		),
 		$contents,
@@ -108,6 +124,8 @@ function render_block_callback( $attributes ) {
 }
 
 /**
+ * Convert to CSS variable.
+ * 
  * Converts a given value to a CSS variable if it starts with 'var:'.
  *
  * @param string $value The value to be converted.
@@ -121,4 +139,75 @@ function convert_to_css_variable( $value ) {
 		}
 	}
 	return $value;
+}
+
+/**
+ * Add CSS property prefix.
+ * 
+ * Adds a prefix to an array of CSS properties.
+ *
+ * @param array $properties The properties to be prefixed.
+ * @param string $prefix The prefix to prepend to properties.
+ * @return array The converted properties.
+ */
+function add_css_property_prefix( array $properties, string $prefix ): array {
+	if ( empty( $properties ) ) {
+		return array();
+	}
+	
+	return array_combine(
+		array_map( 
+			fn( $property ) => "$prefix-$property", 
+			array_keys( $properties )
+		),
+		$properties
+	);
+}
+
+/**
+ * Render inside styles.
+ * 
+ * Collects 
+ *
+ * @param array $styles An associative array of CSS properties and their values.
+ * @return string A string containing inline CSS styles.
+ */
+function render_inline_css( array $styles ): string {
+	$style_strings = array_map(
+		__NAMESPACE__ . "\\render_css_property_string",
+		array_keys( $styles ),
+		array_values( $styles )
+	);
+
+	$validated_style_strings = array_values(
+		array_filter(
+			$style_strings,
+			function( $value ) {
+				return is_string( $value ) && '' !== $value;
+			}
+		)
+	);
+
+	if ( '' === $validated_style_strings ) {
+		return '';
+	}
+
+	return implode( ' ', $validated_style_strings );
+}
+
+/**
+ * Render CSS property string.
+ * 
+ * Converts a property name and value pair into a string to use as CSS.
+ *
+ * @param string $property Style property.
+ * @param string $value Style value.
+ * @return string Property and value CSS string.
+ */
+function render_css_property_string( string $property, string $value ): string {
+	if ( empty( $value ) ) {
+		return '';
+	}
+
+	return sprintf( '%s: %s;', $property, $value );
 }
